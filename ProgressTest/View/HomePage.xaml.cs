2605d096b3f32479dbc9df261a7e068b6b684895
180cms <bcs21090018@student.uts.edu.my>
2023-12-08 13:31:59 +0800

    Revision Push

--- ProgressTest/View/HomePage.xaml.cs
+++ ProgressTest/View/HomePage.xaml.cs
@@ -1,9 +1,27 @@
 namespace ProgressTest.View;
+using CommunityToolkit.Maui.Views;
+
 
 public partial class HomePage : ContentPage
 {
 	public HomePage()
 	{
 		InitializeComponent();
-	}
+    }
+
+    public bool IsExpanded = false;
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        if (IsExpanded == false)
+        {
+            FuckingSpan.Text = "Was a Dutch Post-Impressionist painter who is among the most famous and influential artist in the world.";
+            IsExpanded = true;
+        }
+        else if (IsExpanded == true)
+        {
+            FuckingSpan.Text = "Was a Dutch Post-Impressionist...";
+            IsExpanded = false;
+        }
+    }
 }
