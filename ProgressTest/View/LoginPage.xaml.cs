3898426221981b4f422ff6cf0a6af8cd2ddc42a8
180cms <yung7201913@gmail.com>
2024-01-12 12:05:43 +0800

    Login & Register Page Push

---
+++ ProgressTest/View/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ProgressTest.View;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ProgressTest/View/RegisterPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ProgressTest.View;
+
+public partial class RegisterPage : ContentPage
+{
+	public RegisterPage()
+	{
+		InitializeComponent();
+	}
+}
